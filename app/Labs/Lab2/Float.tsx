e35f223203194e55a44578dc9d8d54eaadfb47c3
aatharva22 <atharvapu10@gmail.com>
2025-10-06 23:13:50 -0400

    Added proper ima

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -2,6 +2,7 @@ export default function Float( ) {
     return(<div id="wd-float-divs">
  <h2>Float</h2>
  <div>
+  
    <img className="wd-float-right"
      src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, quisquam repellendus maxime reprehenderit facilis fuga in tenetur vel ipsa similique iusto deserunt nemo reiciendis ipsam autem atque exercitationem eligendi est. Unde quisquam minus voluptate reiciendis consectetur labore enim maxime quia quidem dolor, tempora amet alias ab! Reprehenderit quasi ea, rem rerum quam id voluptatibus facilis architecto, perspiciatis tempore possimus voluptatum aut maxime libero magnam quos cupiditate ipsum magni explicabo cum amet dolore saepe molestias? Rem necessitatibus quisquam minus repudiandae neque eum exercitationem architecto, commodi accusantium eos reiciendis, vel tempora!

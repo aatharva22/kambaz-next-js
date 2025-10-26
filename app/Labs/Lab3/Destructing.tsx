ac17fe27fbbe5e00f5fc7ff4614bbfdc39028cbd
aatharva22 <atharvapu10@gmail.com>
2025-10-26 18:32:37 -0400

    fixed deployment errors

--- app/Labs/Lab3/Destructing.tsx
+++ app/Labs/Lab3/Destructing.tsx
@@ -11,11 +11,11 @@ export default function Destructing() {
      <h3>Object Destructing</h3>
      {/* Here &#123 and &#125 are used for "{" and "}" in html  */}
      const &#123; name, age &#125; =
-           &#123; name: "John", age: 25 &#125;<br /><br />
+           &#123; name: &#123John&#125, age: 25 &#125;<br /><br />
      name = {name}<br />
      age = {age}
      <h3>Array Destructing</h3>
-     const [first, second, third] = ["one","two","three"]<br/><br/>
+     const [first, second, third] = [&#123one&#125,&#123two&#125,&#123three&#125]<br/><br/>
      first = {first}<br />
      second = {second}<br />
      third = {third}<hr />
--- app/Labs/Lab3/FilterFunction.tsx
+++ app/Labs/Lab3/FilterFunction.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable prefer-const */
 export default function FilterFunction() {
-    let numberArray = [1,2,3,4,5,6]
-    let numbersGreaterThan2 = numberArray.filter((a) => a > 2)
-    let evenNumbers = numberArray.filter((a) => a % 2 === 0)
-    let oddNumbers = numberArray.filter((a) => a % 2 !== 0)
+    const numberArray = [1,2,3,4,5,6]
+    const numbersGreaterThan2 = numberArray.filter((a) => a > 2)
+    const evenNumbers = numberArray.filter((a) => a % 2 === 0)
+    const oddNumbers = numberArray.filter((a) => a % 2 !== 0)
     return (
     <div id="wd-filter-function">
       <h4>Filter Function</h4>
